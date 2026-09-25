777aa590a41191ba51504abc6b08b6d2f3014719
feracvdo <123900224+feracvdo@users.noreply.github.com>
2026-09-25 17:53:45 -0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,15 @@
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon-192.png" type="image/png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/favicon-192.png" />
+        <meta name="theme-color" content="#0a6bf4" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
